4c41e35628f5032852e4fb0b926a8e015b6a32a6
pravesh dav <humantorch1932@gmail.com>
2023-12-27 16:45:42 +0530

    cursor animation added in page 1, added favicon,  page 2 added with animations

---
+++ script.js
@@ -0,0 +1,72 @@
+function locomotiveScroll(){
+    gsap.registerPlugin(ScrollTrigger);
+
+// Using Locomotive Scroll from Locomotive https://github.com/locomotivemtl/locomotive-scroll
+
+const locoScroll = new LocomotiveScroll({
+  el: document.querySelector("#main"),
+  smooth: true
+});
+// each time Locomotive Scroll updates, tell ScrollTrigger to update too (sync positioning)
+locoScroll.on("scroll", ScrollTrigger.update);
+
+// tell ScrollTrigger to use these proxy methods for the "#main" element since Locomotive Scroll is hijacking things
+ScrollTrigger.scrollerProxy("#main", {
+  scrollTop(value) {
+    return arguments.length ? locoScroll.scrollTo(value, 0, 0) : locoScroll.scroll.instance.scroll.y;
+  }, // we don't have to define a scrollLeft because we're only scrolling vertically.
+  getBoundingClientRect() {
+    return {top: 0, left: 0, width: window.innerWidth, height: window.innerHeight};
+  },
+  // LocomotiveScroll handles things completely differently on mobile devices - it doesn't even transform the container at all! So to get the correct behavior and avoid jitters, we should pin things with position: fixed on mobile. We sense it by checking to see if there's a transform applied to the container (the LocomotiveScroll-controlled element).
+  pinType: document.querySelector("#main").style.transform ? "transform" : "fixed"
+});
+
+
+// each time the window updates, we should refresh ScrollTrigger and then update LocomotiveScroll. 
+ScrollTrigger.addEventListener("refresh", () => locoScroll.update());
+
+// after everything is set up, refresh() ScrollTrigger and update LocomotiveScroll because padding may have been added for pinning, etc.
+ScrollTrigger.refresh();
+
+}
+locomotiveScroll();
+function page1CursorAnimation() {
+  let orangeCursor = document.querySelector("#orange_cursor");
+  let page1Content = document.querySelector("#page1_content");
+  page1Content.addEventListener("mousemove", (e) => {
+    gsap.to(orangeCursor, {
+      x: e.clientX,
+      y: e.clientY,
+    });
+  });
+  page1Content.addEventListener("mouseenter", () => {
+    gsap.to(orangeCursor, {
+      scale: 1,
+      opacity: 1,
+    });
+  });
+  page1Content.addEventListener("mouseleave", () => {
+    gsap.to(orangeCursor, {
+      scale: 0,
+      opacity: 0,
+    });
+  });
+}
+page1CursorAnimation();
+
+function page2Animation(){
+    gsap.from(".elem h1, #page2_top h3, #page2_top h4",{
+        y: 120,
+        stagger: 0.2,
+        duration: 2,
+        scrollTrigger: {
+            trigger: "#page2",
+            scroller: "#main",
+            start: "top 62%",
+            end: "top 61%",
+            scrub: 7,
+        },
+    });
+};
+page2Animation();
